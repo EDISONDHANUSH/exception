8e2bdd6d5201aba7ad4e4ada37dc16abe1b8a06b
EDISONDHANUSH <edisonabicristen@gmail.com>
2023-06-26 10:32:46 +0530

    updated project

--- UseClass.java
+++ UseClass.java
@@ -5,9 +5,10 @@ public class UseClass {
 	public static void main(String[] args)throws PasswordException {
 		// TODO Auto-generated method stub
 		Login l=new Login();
+		System.out.println(l);
 try {
 	
-	System.out.println(l.CheckedPassword("ediso"));
+	System.out.println(l.CheckedPassword("edisondhanush"));
 }
 catch(Exception e) {
 	System.out.println(e);
